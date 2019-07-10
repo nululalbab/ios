296a0cdc2473837d621e89b5f41146d19a30fb9b
Ulul <nululalbab@gmail.com>
2019-07-10 10:47:11 +0700

    Commit

--- Training/Training/PopupVC.swift
+++ Training/Training/PopupVC.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class PopupVC: UIViewController{
     
+    @IBOutlet weak var viewSquare: UIView!
+    @IBOutlet weak var dismissButton: UIButton!
     override func viewDidLoad() {
       super.viewDidLoad()
         
+        
+        viewSquare.layer.cornerRadius = 10.0
+        dismissButton.layer.cornerRadius = 10.0
     }
     @IBAction func closePopup(_ sender: Any) {
         dismiss(animated: true, completion: nil)
